a1fce27d11eb447f7095e43f6eb25a16c43f11f2
Finlay John <findominator7@gmail.com>
2023-07-28 16:41:14 +0100

    Small Fixes
    Small fixes on various things

--- Assets/Scripts/UI Scripts/MenuFunctions.cs
+++ Assets/Scripts/UI Scripts/MenuFunctions.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuFunctions : MonoBehaviour
 {
-    public void EnterGame()
+
+    public Animator transition;
+    public float transitionTime = 1f;
+    public bool mainMenu;
+
+    public void LoadGame()
     {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(EnterGame(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    public IEnumerator EnterGame(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        if(mainMenu)
+        SceneManager.LoadSceneAsync(levelIndex);
     }
     public void QuitGame()
     {
